7144912794c3d8b9699233172141174d63d414b0
TuaHN <hoangtua.vn@gmail.com>
2021-01-21 23:04:32 -0500

    reorganize user's models

--- hitarget/api/api_v1/endpoints/authentication.py
+++ hitarget/api/api_v1/endpoints/authentication.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-from hitarget.models.user import User, ResponseUser
+from hitarget.models.user import UserInDB, UserInResponse
 from hitarget.core.mongodb import AsyncIOMotorDatabase, get_database
 from hitarget.business.user import create_user
 
@@ -10,9 +10,9 @@ router = APIRouter(prefix='/users')
 
 
 @router.post("/register", response_description="Register a new user account")
-async def register_user(user: User,
+async def register_user(user: UserInDB,
                         db: AsyncIOMotorDatabase = Depends(get_database)):
     created_user = await create_user(db, user)
-    response = ResponseUser(**created_user.dict())
+    response = UserInResponse(**created_user.dict())
     return JSONResponse(status_code=status.HTTP_201_CREATED,
                         content=jsonable_encoder(response))
--- hitarget/models/user.py
+++ hitarget/models/user.py
@@ -6,23 +6,24 @@ from .helper import PyObjectId, ObjectId
 
 class User(BaseModel):
     __collection__: str = 'users'
-    id: Optional[PyObjectId] = Field(alias='_id')
     email: str
-    password: str
-    salt: Optional[str]
     name: Optional[str]
 
-    class Config:
-        allow_population_by_field_name = True
 
-
-class ResponseUser(BaseModel):
+class UserInResponse(User):
     id: Optional[PyObjectId]
-    email: str = ...
-    name: Optional[str]
 
     class Config:
         json_encoders = {
             # The workaround's https://github.com/tiangolo/fastapi/issues/1515
             ObjectId: lambda v: str(v),
         }
+
+
+class UserInDB(User):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    password: str
+    salt: Optional[str]
+
+    class Config:
+        allow_population_by_field_name = True
